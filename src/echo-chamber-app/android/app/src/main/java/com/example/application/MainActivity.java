8d451080871a46b3c53a1b0a3d4e966fdd0885ca
ivol <ivol@criticalblue.com>
2021-11-05 13:59:32 +0000

    Working Android app Without Approov SDK package

---
+++ src/echo-chamber-app/android/app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.application;
+
+//import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
+//import io.flutter.app.FlutterActivity;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  
+}
